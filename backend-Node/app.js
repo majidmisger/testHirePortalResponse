a3ed86d62d6f822217c94d51cee2ced724f4158e
majid <majidmisger42@gmail.com>
2023-09-19 10:30:14 +0530

    static on Json Backend

--- backend-Node/app.js
+++ backend-Node/app.js
@@ -6,6 +6,9 @@ const ActiveJob = require('./models/activeJobData');
 const CountJob = require('./models/countJobs')
 const cors = require('cors')
 
+//using this file temporary to avoid using db 
+const { ActiveJobData, countJob }=require("./tempData/staticData")
+
 const app = express();
 const port = 8000;
 
@@ -29,13 +32,19 @@ mongoose.connect(process.env.dbUrl, {
 
 app.get('/api/active-jobs', async (req, res) => {
   try {
-  // Fetch data from the 'activejobs' collection
-  const activeJobsData = await ActiveJob.find({});
+  
+    // Fetch data from the 'activejobs' collection
+  // const activeJobsData = await ActiveJob.find({});                   //for using database 
+  const activeJobsData =await ActiveJobData                                    //without database
+
+
 
   // Fetch data from the 'countJobs' collection
-  const countJobsData = await CountJob.findOne({ type: 'Active Jobs' });
+  // const countJobsData = await CountJob.findOne({ type: 'Active Jobs' });            //for using database
+  const countJobsData = await countJob                                                         //without database
 
-  // Create the desired response format
+
+  t
   const response = {
     type: countJobsData.type,
     countPerDay: countJobsData.countPerDay,
